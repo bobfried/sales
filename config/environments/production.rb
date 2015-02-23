3f142ce7ccbb5eecb15acb706bdbae8f8a7780aa
Bob Fried <bobfried@centurylink.net>
2015-02-23 09:59:22 -0600

    More ':'s in the right place  Changes to be committed: 	modified:   config/environments/production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -101,10 +101,10 @@ Rails.application.configure do
     :storage => :s3,
     :url => ':s3_domain_url',
     :path => '/:class/:attachment/:id_partition/:style/:filename',
-    s3_credentials: {
-      bucket: => ENV['AWS_BUCKET'],
-      access_key_id: => ENV['AWS_ACCESS_KEY_ID'],
-      secret_access_key: => ENV['AWS_SECRET_ACCESS_KEY']
+    :s3_credentials {
+      :bucket => ENV['AWS_BUCKET'],
+      :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
+      :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
     }
   }
 end
