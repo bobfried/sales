4d629530dedefaabbac2449f4309d7596436a4b4
Bob Fried <bobfried@centurylink.net>
2015-02-23 10:04:49 -0600

    Fixed Another '=>'  Changes to be committed: 	modified:   config/environments/production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -101,7 +101,7 @@ Rails.application.configure do
     :storage => :s3,
     :url => ':s3_domain_url',
     :path => '/:class/:attachment/:id_partition/:style/:filename',
-    :s3_credentials {
+    :s3_credentials => {
       :bucket => ENV['AWS_BUCKET'],
       :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
       :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
